208112a15f651d428b75cfd74af959f035be8407
SuperCobianzo <cobianzo@gmail.com>
2015-03-04 19:49:37 +0100

    soft scroll and refresh cache

--- mapplic/js/smoothscroll.js
+++ mapplic/js/smoothscroll.js
@@ -533,3 +533,27 @@ if (wheelEvent && isChrome) {
 }
 
 })();
+
+
+
+
+
+// esto es para q el scroll de la pag tb sea smooth
+$(document).ready(function(){
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .stop().animate({scrollTop: targetOffset}, 1300, 'easeOutBounce');
+       return false;
+      }
+    }
+  });
+
+	
+});
